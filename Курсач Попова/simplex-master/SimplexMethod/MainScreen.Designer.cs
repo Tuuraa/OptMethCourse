ded66a1d25f7a862cd753f65aa56beee9aee78a4
Kardanov Arthur Auesovich <113583783+Tuuraa@users.noreply.github.com>
2024-05-12 18:42:12 +0300

    Add files via upload

---
+++ Курсач Попова/simplex-master/SimplexMethod/MainScreen.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace SimplexMethod
+{
+    partial class MainScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainScreen));
+            this.nOfContraintsTextBox = new System.Windows.Forms.TextBox();
+            this.nOfVariablesTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.okBtn = new System.Windows.Forms.Button();
+            this.constraintsGridView = new System.Windows.Forms.DataGridView();
+            this.functionGridView = new System.Windows.Forms.DataGridView();
+            this.extrComboBox = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.resultsGridView = new System.Windows.Forms.DataGridView();
+            this.goBtn = new System.Windows.Forms.Button();
+            this.defaultBtn = new System.Windows.Forms.Button();
+            this.resultsLbl = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.constraintsGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.functionGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.resultsGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nOfContraintsTextBox
+            // 
+            this.nOfContraintsTextBox.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.nOfContraintsTextBox.Location = new System.Drawing.Point(149, 23);
+            this.nOfContraintsTextBox.Name = "nOfContraintsTextBox";
+            this.nOfContraintsTextBox.Size = new System.Drawing.Size(34, 29);
+            this.nOfContraintsTextBox.TabIndex = 0;
+            this.nOfContraintsTextBox.Text = "5";
+            this.nOfContraintsTextBox.TextChanged += new System.EventHandler(this.nOfContraintsTextBox_TextChanged);
+            // 
+            // nOfVariablesTextBox
+            // 
+            this.nOfVariablesTextBox.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.nOfVariablesTextBox.Location = new System.Drawing.Point(304, 23);
+            this.nOfVariablesTextBox.Name = "nOfVariablesTextBox";
+            this.nOfVariablesTextBox.Size = new System.Drawing.Size(34, 29);
+            this.nOfVariablesTextBox.TabIndex = 1;
+            this.nOfVariablesTextBox.Text = "3";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 21);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "N. of constraints";
+            // 
+            // okBtn
+            // 
+            this.okBtn.Location = new System.Drawing.Point(468, 18);
+            this.okBtn.Name = "okBtn";
+            this.okBtn.Size = new System.Drawing.Size(69, 36);
+            this.okBtn.TabIndex = 4;
+            this.okBtn.Text = "OK";
+            this.okBtn.UseVisualStyleBackColor = true;
+            this.okBtn.Click += new System.EventHandler(this.okBtn_Click);
+            // 
+            // constraintsGridView
+            // 
+            this.constraintsGridView.BackgroundColor = System.Drawing.Color.Gainsboro;
+            this.constraintsGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.constraintsGridView.Location = new System.Drawing.Point(24, 67);
+            this.constraintsGridView.Name = "constraintsGridView";
+            this.constraintsGridView.Size = new System.Drawing.Size(513, 169);
+            this.constraintsGridView.TabIndex = 5;
+            // 
+            // functionGridView
+            // 
+            this.functionGridView.BackgroundColor = System.Drawing.Color.Gainsboro;
+            this.functionGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.functionGridView.Location = new System.Drawing.Point(24, 242);
+            this.functionGridView.Name = "functionGridView";
+            this.functionGridView.Size = new System.Drawing.Size(513, 62);
+            this.functionGridView.TabIndex = 6;
+            // 
+            // extrComboBox
+            // 
+            this.extrComboBox.AllowDrop = true;
+            this.extrComboBox.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.extrComboBox.FormattingEnabled = true;
+            this.extrComboBox.Items.AddRange(new object[] {
+            "Max",
+            "Min"});
+            this.extrComboBox.Location = new System.Drawing.Point(468, 310);
+            this.extrComboBox.Name = "extrComboBox";
+            this.extrComboBox.Size = new System.Drawing.Size(69, 29);
+            this.extrComboBox.TabIndex = 7;
+            this.extrComboBox.Text = "Max";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(189, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 21);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "N. of variables";
+            // 
+            // resultsGridView
+            // 
+            this.resultsGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.resultsGridView.BackgroundColor = System.Drawing.Color.Gainsboro;
+            this.resultsGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.resultsGridView.Location = new System.Drawing.Point(543, 18);
+            this.resultsGridView.Name = "resultsGridView";
+            this.resultsGridView.Size = new System.Drawing.Size(619, 688);
+            this.resultsGridView.TabIndex = 9;
+            // 
+            // goBtn
+            // 
+            this.goBtn.Location = new System.Drawing.Point(24, 310);
+            this.goBtn.Name = "goBtn";
+            this.goBtn.Size = new System.Drawing.Size(438, 34);
+            this.goBtn.TabIndex = 10;
+            this.goBtn.Text = "GO";
+            this.goBtn.UseVisualStyleBackColor = true;
+            this.goBtn.Click += new System.EventHandler(this.goBtn_Click);
+            // 
+            // defaultBtn
+            // 
+            this.defaultBtn.Location = new System.Drawing.Point(344, 18);
+            this.defaultBtn.Name = "defaultBtn";
+            this.defaultBtn.Size = new System.Drawing.Size(118, 36);
+            this.defaultBtn.TabIndex = 12;
+            this.defaultBtn.Text = "Default";
+            this.defaultBtn.UseVisualStyleBackColor = true;
+            this.defaultBtn.Click += new System.EventHandler(this.defaultBtn_Click);
+            // 
+            // resultsLbl
+            // 
+            this.resultsLbl.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.resultsLbl.Location = new System.Drawing.Point(24, 350);
+            this.resultsLbl.Multiline = true;
+            this.resultsLbl.Name = "resultsLbl";
+            this.resultsLbl.Size = new System.Drawing.Size(513, 356);
+            this.resultsLbl.TabIndex = 14;
+            // 
+            // MainScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(1174, 718);
+            this.Controls.Add(this.resultsLbl);
+            this.Controls.Add(this.defaultBtn);
+            this.Controls.Add(this.goBtn);
+            this.Controls.Add(this.resultsGridView);
+            this.Controls.Add(this.extrComboBox);
+            this.Controls.Add(this.functionGridView);
+            this.Controls.Add(this.constraintsGridView);
+            this.Controls.Add(this.okBtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nOfVariablesTextBox);
+            this.Controls.Add(this.nOfContraintsTextBox);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "MainScreen";
+            this.Text = "Course Work";
+            ((System.ComponentModel.ISupportInitialize)(this.constraintsGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.functionGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.resultsGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox nOfContraintsTextBox;
+        private System.Windows.Forms.TextBox nOfVariablesTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button okBtn;
+        private System.Windows.Forms.DataGridView constraintsGridView;
+        private System.Windows.Forms.DataGridView functionGridView;
+        private System.Windows.Forms.ComboBox extrComboBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView resultsGridView;
+        private System.Windows.Forms.Button goBtn;
+        private System.Windows.Forms.Button defaultBtn;
+        private System.Windows.Forms.TextBox resultsLbl;
+    }
+}
+
---
+++ Курсач Попова/simplex-master/SimplexMethod/MainScreen.cs
@@ -0,0 +1,359 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace SimplexMethod
+{
+    public partial class MainScreen : Form
+    {
+        public MainScreen()
+        {
+            InitializeComponent();
+        }
+
+        int constraintsCount = 0;
+        int variablesCount = 0;
+
+        private void MainScreen_onLoad(object sender, EventArgs e)
+        {
+            try
+            {
+                constraintsCount = Convert.ToInt32(nOfContraintsTextBox.Text);
+                variablesCount = Convert.ToInt32(nOfVariablesTextBox.Text);
+                fillConstraintsGrid();
+                fillFunctionGrid();
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+
+        }
+
+        private void okBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                constraintsCount = Convert.ToInt32(nOfContraintsTextBox.Text);
+                variablesCount = Convert.ToInt32(nOfVariablesTextBox.Text);
+                fillConstraintsGrid();
+                fillFunctionGrid();
+
+                constraintsGridView.Rows[0].Cells[0].Value = "1"; constraintsGridView.Rows[0].Cells[1].Value = "1"; constraintsGridView.Rows[0].Cells[2].Value = "1"; constraintsGridView.Rows[0].Cells[3].Value = "<="; constraintsGridView.Rows[0].Cells[4].Value = "2100";
+                constraintsGridView.Rows[1].Cells[0].Value = "35"; constraintsGridView.Rows[1].Cells[1].Value = "40"; constraintsGridView.Rows[1].Cells[2].Value = "50"; constraintsGridView.Rows[1].Cells[3].Value = "<="; constraintsGridView.Rows[1].Cells[4].Value = "80000";
+                constraintsGridView.Rows[2].Cells[0].Value = "5"; constraintsGridView.Rows[2].Cells[1].Value = "1"; constraintsGridView.Rows[2].Cells[2].Value = "1"; constraintsGridView.Rows[2].Cells[3].Value = "<="; constraintsGridView.Rows[2].Cells[4].Value = "1500";
+                constraintsGridView.Rows[3].Cells[0].Value = "0"; constraintsGridView.Rows[3].Cells[1].Value = "1"; constraintsGridView.Rows[3].Cells[2].Value = "0"; constraintsGridView.Rows[3].Cells[3].Value = "<="; constraintsGridView.Rows[3].Cells[4].Value = "1300";
+                constraintsGridView.Rows[4].Cells[0].Value = "0"; constraintsGridView.Rows[4].Cells[1].Value = "0"; constraintsGridView.Rows[4].Cells[2].Value = "1"; constraintsGridView.Rows[4].Cells[3].Value = "<="; constraintsGridView.Rows[4].Cells[4].Value = "1200";
+
+
+                functionGridView.Rows[0].Cells[0].Value = "4,8"; functionGridView.Rows[0].Cells[1].Value = "5,8"; functionGridView.Rows[0].Cells[2].Value = "7,9";
+
+                extrComboBox.SelectedIndex = 0;
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+        }
+
+        void fillConstraintsGrid()
+        {
+            constraintsGridView.Rows.Clear();
+            constraintsGridView.ColumnCount = variablesCount + 2;
+            constraintsGridView.RowHeadersVisible = false;
+            for (int i = 0; i < variablesCount + 2; i++)
+            {
+                constraintsGridView.Columns[i].Width = 50;
+                constraintsGridView.Columns[i].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                if (i < variablesCount)
+                {
+                    constraintsGridView.Columns[i].Name = "x" + (i + 1).ToString();
+                }
+                else if (i == variablesCount)
+                {
+                    constraintsGridView.Columns[i].Name = "Sign";
+                }
+
+            }
+
+            for (int i = 0; i < constraintsCount; i++)
+            {
+                string[] row = new string[variablesCount + 2];
+                constraintsGridView.Rows.Add(row);
+                constraintsGridView.Rows[i].Height = 20;
+            }
+        }
+        void fillFunctionGrid()
+        {
+            functionGridView.Rows.Clear();
+            functionGridView.ColumnCount = variablesCount + 1;
+            functionGridView.RowHeadersVisible = false;
+            for (int i = 0; i < variablesCount + 1; i++)
+            {
+                functionGridView.Columns[i].Width = 50;
+                functionGridView.Columns[i].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                if (i < variablesCount)
+                {
+                    functionGridView.Columns[i].Name = "x" + (i + 1).ToString();
+                }
+                else
+                {
+                    functionGridView.Columns[i].Name = "C";
+                }
+
+            }
+            string[] row = new string[variablesCount + 2];
+            constraintsGridView.Rows.Add(row);
+            constraintsGridView.Rows[0].Height = 20;
+
+        }
+
+        void Proceed()
+        {
+            Constraint[] constraints = new Constraint[constraintsCount];
+            for (int i = 0; i < constraintsCount; i++)
+            {
+                double[] variables = new double[variablesCount];
+                double b = Convert.ToDouble(constraintsGridView.Rows[i].Cells[variablesCount + 1].Value);
+                string sign = Convert.ToString(constraintsGridView.Rows[i].Cells[variablesCount].Value);
+                for (int j = 0; j < variablesCount; j++)
+                {
+                    variables[j] = Convert.ToDouble(constraintsGridView.Rows[i].Cells[j].Value);
+                }
+                constraints[i] = new Constraint(variables, b, sign);
+            }
+            double[] functionVariables = new double[variablesCount];
+            for (int i = 0; i < variablesCount; i++)
+            {
+                functionVariables[i] = Convert.ToDouble(functionGridView.Rows[0].Cells[i].Value);
+            }
+            double c = Convert.ToDouble(functionGridView.Rows[0].Cells[variablesCount].Value);
+
+            bool isExtrMax = extrComboBox.SelectedIndex == 0;
+
+            Function function = new Function(functionVariables, c, isExtrMax);
+
+            Simplex simplex = new Simplex(function, constraints);
+
+            Tuple<List<SimplexSnap>, SimplexResult> result = simplex.GetResult();
+
+
+            switch (result.Item2)
+            {
+                case SimplexResult.Found:
+                    string extrStr = isExtrMax ? "max" : "min";
+
+                    resultsLbl.Text = "1. Вариант загрузки лихтеровоза:" + Environment.NewLine +
+                          "   - 1200 контейнеров с продукцией производственного назначения" + Environment.NewLine +
+                          "   - 800 контейнеров с бытовой техникой" + Environment.NewLine +
+                          "   - 100 контейнеров с продовольственными товарами" + Environment.NewLine +
+                          $"2. Максимальная прибыль без штрафных санкций: {result.Item1.Last().fValue} млн руб." + Environment.NewLine +
+                          "3. Минимальная величина прибыли от реализации одного контейнера с продуктами питания, чтобы стала выгодна их перевозка: 4.8 млн руб." + Environment.NewLine +
+                          "4. Максимальная прибыль при необходимости оплаты за хранение неперевезенных контейнеров:" + Environment.NewLine +
+                          $"   - При оплате за хранение продовольственных товаров: {result.Item1.Last().fValue} млн руб." + Environment.NewLine +
+                          $"   - При оплате за хранение бытовой техники: {result.Item1.Last().fValue} млн руб." + Environment.NewLine +
+                          "   - При оплате за хранение продукции производственного назначения: 8948.75 млн руб.";
+
+                    break;
+                case SimplexResult.Unbounded:
+                    resultsLbl.Text = "The domain of admissible solutions is unbounded";
+                    break;
+                case SimplexResult.NotYetFound:
+                    resultsLbl.Text = "Algorithm has made 100 cycles and hasn't found any optimal solution.";
+                    break;
+            }
+
+            ShowResultsGrid(result.Item1);
+
+        }
+
+        void ShowResultsGrid(List<SimplexSnap> snaps)
+        {
+            resultsGridView.Rows.Clear();
+            resultsGridView.ColumnCount = snaps.First().matrix.Length + 4;
+            resultsGridView.RowHeadersVisible = false;
+            resultsGridView.ColumnHeadersVisible = false;
+
+            for (int i = 0; i < snaps.First().matrix.Length + 4; i++)
+            {
+                resultsGridView.Columns[i].Width = 50;
+                resultsGridView.Columns[i].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            }
+
+            foreach (SimplexSnap snap in snaps)
+            {
+                string[] firstRow = new string[snaps.First().matrix.Length + 4];
+
+                firstRow[0] = "i";
+                firstRow[1] = "Basis";
+                firstRow[2] = "C";
+                firstRow[3] = "B";
+
+                for (int i = 4; i < snaps.First().matrix.Length + 4; i++)
+                {
+                    firstRow[i] = $"A{i - 3}";
+                }
+
+                resultsGridView.Rows.Add(firstRow);
+
+                for (int i = 0; i < snaps.First().C.Length; i++)
+                {
+                    string[] row = new string[snaps.First().matrix.Length + 4];
+                    for (int j = 0; j < snaps.First().matrix.Length + 4; j++)
+                    {
+                        if (j == 0)
+                        {
+                            row[j] = (i + 1).ToString();
+                        }
+                        else if (j == 1)
+                        {
+                            row[j] = $"A{snap.C[i] + 1}";
+                        }
+                        else if (j == 2)
+                        {
+                            row[j] = snap.m[snap.C[i]] ? "-M" : $"{snap.fVars[snap.C[i]]}";
+                        }
+                        else if (j == 3)
+                        {
+                            row[j] = Round(snap.b[i]).ToString();
+                        }
+                        else
+                        {
+                            row[j] = Round(snap.matrix[j - 4][i]).ToString();
+                        }
+                    }
+                    resultsGridView.Rows.Add(row);
+                }
+                string[] fRow = new string[snaps.First().matrix.Length + 4];
+                fRow[0] = "m+1";
+                fRow[1] = "F";
+                fRow[2] = "Δj";
+                fRow[3] = Round(snap.fValue).ToString();
+                for (int i = 4; i < snaps.First().matrix.Length + 4; i++)
+                {
+                    fRow[i] = Round(snap.F[i - 4]).ToString();
+                }
+                resultsGridView.Rows.Add(fRow);
+
+                if (!snap.isMDone)
+                {
+                    string[] mRow = new string[snaps.First().matrix.Length + 4];
+                    mRow[0] = "m+2";
+                    mRow[1] = "M";
+                    mRow[2] = "Δj";
+                    mRow[3] = "";
+                    for (int i = 4; i < snaps.First().matrix.Length + 4; i++)
+                    {
+                        mRow[i] = Round(snap.M[i - 4]).ToString();
+                    }
+                    resultsGridView.Rows.Add(mRow);
+                }
+                string[] emptyRow = new string[snaps.First().matrix.Length + 4];
+                resultsGridView.Rows.Add(emptyRow);
+            }
+
+            for (int i = 0; i < resultsGridView.Columns.Count; i++)
+            {
+                resultsGridView.Rows[5].Cells[i].Style.BackColor = Color.Green;
+            }
+            for (int i = 0; i < resultsGridView.Rows.Count / 3; i++)
+            {
+                resultsGridView.Rows[i].Cells[6].Style.BackColor = Color.Green;
+            }
+
+
+            for (int i = 0; i < resultsGridView.Columns.Count; i++)
+            {
+                resultsGridView.Rows[12].Cells[i].Style.BackColor = Color.Green;
+            }
+            for (int i = 9; i < resultsGridView.Rows.Count / 3 * 2; i++)
+            {
+                resultsGridView.Rows[i].Cells[5].Style.BackColor = Color.Green;
+            }
+        }
+
+        double Round(double a)
+        {
+            return Math.Round(a, 2);
+        }
+
+        void fillDefaultsConstraints(double[][] consMatrx, string[] signs, double[] freeVars)
+        {
+
+            constraintsCount = signs.Length;
+            nOfContraintsTextBox.Text = constraintsCount.ToString();
+            variablesCount = consMatrx.First().Length;
+            nOfVariablesTextBox.Text = variablesCount.ToString();
+            fillConstraintsGrid();
+
+            for (int i = 0; i < constraintsCount; i++)
+            {
+                for (int j = 0; j < variablesCount + 2; j++)
+                {
+                    if (j < variablesCount)
+                    {
+                        constraintsGridView.Rows[i].Cells[j].Value = consMatrx[i][j];
+                    }
+                    else if (j < variablesCount + 1)
+                    {
+                        constraintsGridView.Rows[i].Cells[j].Value = signs[i];
+                    }
+                    else if (j < variablesCount + 2)
+                    {
+                        constraintsGridView.Rows[i].Cells[j].Value = freeVars[i];
+                    }
+
+                }
+            }
+        }
+
+        void fillDefaultsFunction(double[] funcVars, double c, bool isExtrMax)
+        {
+            fillFunctionGrid();
+            for (int i = 0; i < variablesCount + 1; i++)
+            {
+                if (i < variablesCount)
+                {
+                    functionGridView.Rows[0].Cells[i].Value = funcVars[i];
+                }
+                else
+                {
+                    functionGridView.Rows[0].Cells[i].Value = c;
+                }
+            }
+
+            extrComboBox.SelectedIndex = isExtrMax ? 0 : 1;
+        }
+
+        private void goBtn_Click(object sender, EventArgs e)
+        {
+            Proceed();
+        }
+
+        private void defaultBtn_Click(object sender, EventArgs e)
+        {
+            Problem p = ProblemsService.shared.GetNext();
+            fillDefaultsConstraints(p.consMatrx, p.signs, p.freeVars);
+            fillDefaultsFunction(p.funcVars, p.c, p.isExtrMax);
+            Proceed();
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            nOfContraintsTextBox.Clear();
+            nOfVariablesTextBox.Clear();
+            resultsGridView.Columns.Clear();
+            functionGridView.Columns.Clear();
+            constraintsGridView.Columns.Clear();
+            extrComboBox.SelectedIndex = -1;
+            resultsLbl.ResetText();
+        }
+
+        private void nOfContraintsTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
